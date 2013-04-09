bc9fa4eeba4e03721e1b5cdd36f7d966c03660d2
Shekhar <pythonic@gmail.com>
2013-04-09 13:56:49 +0530

    no duplicate bash_aliases lines in bashrc

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo apt-get install exuberant-ctags git
 
-echo "source .bash_aliases" >> ~/.bashrc
+grep 'bash_aliases' ~/.bashrc || echo "source ~/.bash_aliases" >> ~/.bashrc
 ln -sf `pwd`/.bash_aliases ~/.bash_aliases
 ln -sf `pwd`/.vimrc ~/.vimrc
 ln -sf `pwd`/.vim ~/.vim
