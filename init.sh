beed6c6eb7d5e3633ace1285fd89abcc008dba1b
Shekhar <pythonic@gmail.com>
2013-04-09 10:05:33 +0530

    overwriting existing links is ok

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo apt-get install exuberant-ctags git
 
 echo "source .bash_aliases" >> ~/.bashrc
-ln -s `pwd`/.bash_aliases ~/.bash_aliases
-ln -s `pwd`/.vimrc ~/.vimrc
-ln -s `pwd`/.vim ~/.vim
+ln -sf `pwd`/.bash_aliases ~/.bash_aliases
+ln -sf `pwd`/.vimrc ~/.vimrc
+ln -sf `pwd`/.vim ~/.vim
